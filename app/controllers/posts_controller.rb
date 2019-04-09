09ebbf466ca6415de6d913328d30ffae0e1664ff
Neil Seejoor <nvseejoor@gmail.com>
2019-04-09 00:53:41 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -31,10 +31,10 @@ class PostsController < ApplicationController
     redirect_to post_path(@post)
   end
 
-  def post_data
-    post = Post.find(params[:id])
-    render json: post.to_json(only: [:title, :description, :id], include: [author: {only: [:name]}])
-  end
+  # def post_data
+  #   post = Post.find(params[:id])
+  #   render json: post.to_json(only: [:title, :description, :id], include: [author: {only: [:name]}])
+  # end
 
   private
 
